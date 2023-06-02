fffb9f0b39b5cef4f37419251b8fc6b3b017b17c
nativacu <nativacu@gmail.com>
2023-06-02 10:07:28 -0400

    chore: debugging routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MockStore';
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    this.logCurrentRoute();
+  }
+
+  logCurrentRoute() {
+    this.router.events.subscribe(event => {
+      console.log(this.router.url);
+    });
+  }
 }
