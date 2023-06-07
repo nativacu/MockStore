cac42725dbb69fb06f5896595e7771df6726607e
nativacu <nativacu@gmail.com>
2023-06-07 12:20:57 -0400

    chore: removing route logs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
   title = 'MockStore';
-
-  constructor(private router: Router) { }
-
-  ngOnInit() {
-    this.logCurrentRoute();
-  }
-
-  logCurrentRoute() {
-    this.router.events.subscribe(event => {
-      console.log(this.router.url);
-    });
-  }
 }
